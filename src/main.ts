0a6d5523ae0bf81b6cb7bdc1d40379fd22f427d1
ousmane <ousmanely11@gmail.com>
2024-09-19 22:05:37 +0000

    Ajout des icons

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
+import router from './router/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import FontAwesomeIcon from './plugins/fontawesome';
+import FontAwesomeIcon from './plugins/fontawesome.js';
 
 const app = createApp(App);
 
---
+++ src/types.d.ts
@@ -0,0 +1,12 @@
+// src/types.d.ts
+
+declare module './router' {
+    const router: any;
+    export default router;
+  }
+  
+  declare module './plugins/fontawesome' {
+    const FontAwesomeIcon: any;
+    export default FontAwesomeIcon;
+  }
+  
