29cbcad0f9beb47425429b45501485d9a0cdc17e
ousmane <ousmanely11@gmail.com>
2024-09-19 22:33:17 +0000

    Mise a jour du fichier readme pour ajouter le lien vercel

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import App from './App.vue';
 import router from './router/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-// import FontAwesomeIcon from './plugins/fontawesome.js';
+import FontAwesomeIcon from './plugins/fontawesome.js';
 
 const app = createApp(App);
 
-// app.component('font-awesome-icon', FontAwesomeIcon);
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.use(router);
 
